e93b674191ca3dc2c90e7599ced7adc9fb31f372
Raine Stone <samstone9001@gmail.com>
2024-09-27 17:12:02 -0400

    cleanup

--- src/main/java/stone/hyperquarry/client/GuiFilter.java
+++ src/main/java/stone/hyperquarry/client/GuiFilter.java
@@ -52,10 +52,10 @@ public class GuiFilter extends GuiScreen {
         int startY = (this.height - this.ySize) / 2 + this.ySize;
 
         this.cancelButton = this
-            .addButton(new GuiButton(0, startX, startY, this.xSize / 2, 20, "Cancel"));
+                .addButton(new GuiButton(0, startX, startY, this.xSize / 2, 20, "Cancel"));
         this.confirmButton = this
-            .addButton(
-                new GuiButton(1, startX + this.xSize / 2, startY, this.xSize / 2, 20, "Confirm"));
+                .addButton(
+                        new GuiButton(1, startX + this.xSize / 2, startY, this.xSize / 2, 20, "Confirm"));
     }
 
     @Override
@@ -67,7 +67,9 @@ public class GuiFilter extends GuiScreen {
         this.drawFilter();
 
         super.drawScreen(mouseX, mouseY, partialTicks);
-        this.drawString(fontRenderer, "left, right, or middle click for on, off, or toggling of items, click dragging is supported", i, j - 20, 0xFFFFFFFF);
+        this.drawString(fontRenderer,
+                "left, right, or middle click for on, off, or toggling of items, click dragging is supported", i,
+                j - 20, 0xFFFFFFFF);
         this.drawString(fontRenderer, "green means make this item, red means trash this item",
                 i, j + ySize - 14, 0xFFFFFFFF);
     }
@@ -75,107 +77,100 @@ public class GuiFilter extends GuiScreen {
     public void drawFilter() {
         int startX = (this.width - this.xSize) / 2;
         int startY = (this.height - this.ySize) / 2;
-        for (int j = 0; j < ROWS; j++)
-        {
-        for (int i = 0; i < COLUMNS; i++)
-        {
-            
+        for (int j = 0; j < ROWS; j++) {
+            for (int i = 0; i < COLUMNS; i++) {
+
                 int x = startX + i * ITEM_SIZE;
                 int y = startY + j * ITEM_SIZE;
-                if (list.items().length > flatten(i, j))
-                {
-                    if (this.filter.mask().get(flatten(i, j)))
-                    {
+                if (list.items().length > flatten(i, j)) {
+                    if (this.filter.mask().get(flatten(i, j))) {
                         Gui.drawRect(x, y, x + ITEM_SIZE, y + ITEM_SIZE, 0xFF00FF00);
-                    } else
-                    {
+                    } else {
                         Gui.drawRect(x, y, x + ITEM_SIZE, y + ITEM_SIZE, 0xFFFF0000);
                     }
                     this.itemRender.renderItemIntoGUI(list.getStack(flatten(i, j), (byte) 1), x, y);
-                } else
-                {
+                } else {
                     Gui.drawRect(x, y, x + ITEM_SIZE, y + ITEM_SIZE, 0xA0000000);
                 }
             }
-        //if (list.items().length <= flatten(0, j))
-        //return;
         }
     }
-    
+
     private BitSet toggled = new BitSet();
+
     @Override
     protected void mouseClicked(int mouseX, int mouseY, int mouseButton) throws IOException {
         super.mouseClicked(mouseX, mouseY, mouseButton);
         toggled.clear();
         flipSlot(mouseX, mouseY, mouseButton);
     }
-    
+
     @Override
-	protected void mouseClickMove(int mouseX, int mouseY, int clickedMouseButton, long timeSinceLastClick) {
-		super.mouseClickMove(mouseX, mouseY, clickedMouseButton, timeSinceLastClick);
-		
-		flipSlot(mouseX, mouseY, clickedMouseButton);
-	}
-    
+    protected void mouseClickMove(int mouseX, int mouseY, int clickedMouseButton, long timeSinceLastClick) {
+        super.mouseClickMove(mouseX, mouseY, clickedMouseButton, timeSinceLastClick);
+
+        flipSlot(mouseX, mouseY, clickedMouseButton);
+    }
+
     private void flipSlot(int mouseX, int mouseY, int button) {
-    	// normalize to be in the space of the 2d arrow of items
+        // normalize to be in the space of the 2d arrow of items
         int startX = (this.width - this.xSize) / 2;
         int startY = (this.height - this.ySize) / 2;
 
         int normalizedX = (mouseX - startX) / ITEM_SIZE;
         int normalizedY = (mouseY - startY) / ITEM_SIZE;
 
-        if (normalizedX >= 0 && normalizedX < COLUMNS)
-        {
-            if (normalizedY >= 0 && normalizedY < ROWS)
-            {
+        if (normalizedX >= 0 && normalizedX < COLUMNS) {
+            if (normalizedY >= 0 && normalizedY < ROWS) {
                 int flattened = flatten(normalizedX, normalizedY);
                 if (flattened >= this.list.items().length)
                     return;
-            	if (!toggled.get(flattened)) {
-            	toggled.set(flattened);
-            	if (button == 0) {
-            	this.filter.mask()
-                    .set(flattened, true);
-            	} else if (button == 1) {
-            		this.filter.mask()
-                    .set(flattened, false);
-            	} else if (button == 2) {
-            		this.filter.mask()
-                    .flip(flattened);
-            	}
-            	}
+                if (!toggled.get(flattened)) {
+                    toggled.set(flattened);
+                    if (button == 0) {
+                        this.filter.mask()
+                                .set(flattened, true);
+                    } else if (button == 1) {
+                        this.filter.mask()
+                                .set(flattened, false);
+                    } else if (button == 2) {
+                        this.filter.mask()
+                                .flip(flattened);
+                    }
+                }
             }
         }
     }
 
-	@Override
+    @Override
     protected void keyTyped(char typedChar, int keyCode) {
         if (this.mc.gameSettings.keyBindInventory.isActiveAndMatches(keyCode)) {
             this.mc.displayGuiScreen(lastScreen);
-        }
-        else if (keyCode == 1)
-        { this.mc.player.closeScreen(); }
-    }
-
-	@Override
-    protected void actionPerformed(GuiButton button) throws IOException {
-        super.actionPerformed(button);
-
-        if (button == this.cancelButton)
-        {
-            this.mc.displayGuiScreen(lastScreen);
-        } else if (button == this.confirmButton)
-        {
-            Proxy.NETWORK
-                .sendToServer(
-                    new PacketSetMask(filter, this.target));
+        } else if (keyCode == 1) {
             this.mc.player.closeScreen();
         }
     }
 
-    private int flatten(int i, int j) { return i + j * COLUMNS; }
+    @Override
+    protected void actionPerformed(GuiButton button) throws IOException {
+        super.actionPerformed(button);
+
+        if (button == this.cancelButton) {
+            this.mc.displayGuiScreen(lastScreen);
+        } else if (button == this.confirmButton) {
+            Proxy.NETWORK
+                    .sendToServer(
+                            new PacketSetMask(filter, this.target));
+            this.mc.player.closeScreen();
+        }
+    }
+
+    private int flatten(int i, int j) {
+        return i + j * COLUMNS;
+    }
 
     @Override
-    public boolean doesGuiPauseGame() { return false; }
+    public boolean doesGuiPauseGame() {
+        return false;
+    }
 }
--- src/main/java/stone/hyperquarry/client/GuiQuarry.java
+++ src/main/java/stone/hyperquarry/client/GuiQuarry.java
@@ -15,118 +15,119 @@ import java.io.IOException;
 import java.util.BitSet;
 
 public class GuiQuarry extends GuiScreen {
-	private final BlockPos source;
+    private final BlockPos source;
 
-	private int sizeX = 250;
-	private int sizeY = 100;
-	private GuiButton filter;
-	private GuiButton toggleRunning;
-	private GuiButton confirm;
+    private int sizeX = 250;
+    private int sizeY = 100;
+    private GuiButton filter;
+    private GuiButton toggleRunning;
+    private GuiButton confirm;
 
-	private GuiTextField dimensionList;
-	private GuiTextField enchantsList;
+    private GuiTextField dimensionList;
+    private GuiTextField enchantsList;
 
-	private long mined;
-	private boolean isRunning;
-	private int cost;
+    private long mined;
+    private boolean isRunning;
+    private int cost;
 
-	public GuiQuarry(BlockPos source, long mined, boolean isRunning, int cost) {
-		this.source = source;
-		this.mined = mined;
-		this.isRunning = isRunning;
-		this.cost = cost;
-	}
+    public GuiQuarry(BlockPos source, long mined, boolean isRunning, int cost) {
+        this.source = source;
+        this.mined = mined;
+        this.isRunning = isRunning;
+        this.cost = cost;
+    }
 
-	@Override
-	public void drawScreen(int mouseX, int mouseY, float partialTicks) {
-		this.drawDefaultBackground();
-		super.drawScreen(mouseX, mouseY, partialTicks);
+    @Override
+    public void drawScreen(int mouseX, int mouseY, float partialTicks) {
+        this.drawDefaultBackground();
+        super.drawScreen(mouseX, mouseY, partialTicks);
 
-		int startX = (this.width - this.sizeX) / 2;
-		int startY = (this.height - this.sizeY) / 2;
+        int startX = (this.width - this.sizeX) / 2;
+        int startY = (this.height - this.sizeY) / 2;
 
-		this.drawString(fontRenderer, "Total Items Mined: " + mined, startX, startY, 0xFFFFFFFF);
-		this.drawString(fontRenderer, "Current Cost per Item: " + cost, startX, startY + 20, 0xFFFFFF);
-		this.drawString(fontRenderer, "Enchants Name:", startX, startY + 40, 0xFFFFFFFF);
-		if (mouseX > startX && mouseX < startX + this.sizeX / 2) {
-			if (mouseY > startY + 40 && mouseY < startY + 60) {
-				this.drawHoveringText("Valid Names: \"drops\", \"smelts\", \"silks\"", mouseX, mouseY);
-			}
-		}
+        this.drawString(fontRenderer, "Total Items Mined: " + mined, startX, startY, 0xFFFFFFFF);
+        this.drawString(fontRenderer, "Current Cost per Item: " + cost, startX, startY + 20, 0xFFFFFF);
+        this.drawString(fontRenderer, "Enchants Name:", startX, startY + 40, 0xFFFFFFFF);
+        if (mouseX > startX && mouseX < startX + this.sizeX / 2) {
+            if (mouseY > startY + 40 && mouseY < startY + 60) {
+                this.drawHoveringText("Valid Names: \"drops\", \"smelts\", \"silks\"", mouseX, mouseY);
+            }
+        }
 
-                if (mouseX > startX + sizeX / 2 && mouseX < startX + this.sizeX) {
-			if (mouseY > startY + 40 && mouseY < startY + 60) {
-				this.drawHoveringText("Enter a dimension id like 0, 1, -9999, /forge tps can help to find ids", mouseX, mouseY);
-			}
-		}
-                
-		this.enchantsList.drawTextBox();
-		this.drawString(fontRenderer, "Dimension Id:", startX + this.sizeX / 2, startY + 40, 0xFFFFFFFF);
-		this.dimensionList.drawTextBox();
-	}
+        if (mouseX > startX + sizeX / 2 && mouseX < startX + this.sizeX) {
+            if (mouseY > startY + 40 && mouseY < startY + 60) {
+                this.drawHoveringText("Enter a dimension id like 0, 1, -9999, /forge tps can help to find ids", mouseX,
+                        mouseY);
+            }
+        }
 
-	@Override
-	public void initGui() {
-		super.initGui();
-		int id = 0;
+        this.enchantsList.drawTextBox();
+        this.drawString(fontRenderer, "Dimension Id:", startX + this.sizeX / 2, startY + 40, 0xFFFFFFFF);
+        this.dimensionList.drawTextBox();
+    }
 
-		int startX = (this.width - this.sizeX) / 2;
-		int startY = (this.height - this.sizeY) / 2;
-		this.filter = addButton(new GuiButton(id++, startX, startY + this.sizeY, this.sizeX / 2, 20, "Open Filter"));
-		this.toggleRunning = addButton(new GuiButton(id++, startX + this.sizeX / 2, startY + this.sizeY, this.sizeX / 2,
-				20, isRunning ? "Stop" : "Start"));
-		this.confirm = addButton(new GuiButton(id++, startX, startY + 80, this.sizeX, 20, "Confirm Drop List"));
-		;
-		this.enchantsList = new GuiTextField(id++, fontRenderer, startX, startY + 60, this.sizeX / 2, 20);
+    @Override
+    public void initGui() {
+        super.initGui();
+        int id = 0;
 
-		this.dimensionList = new GuiTextField(id++, fontRenderer, startX + this.sizeX / 2, startY + 60, this.sizeX / 2,
-				20);
-	}
+        int startX = (this.width - this.sizeX) / 2;
+        int startY = (this.height - this.sizeY) / 2;
+        this.filter = addButton(new GuiButton(id++, startX, startY + this.sizeY, this.sizeX / 2, 20, "Open Filter"));
+        this.toggleRunning = addButton(new GuiButton(id++, startX + this.sizeX / 2, startY + this.sizeY, this.sizeX / 2,
+                20, isRunning ? "Stop" : "Start"));
+        this.confirm = addButton(new GuiButton(id++, startX, startY + 80, this.sizeX, 20, "Confirm Drop List"));
+        ;
+        this.enchantsList = new GuiTextField(id++, fontRenderer, startX, startY + 60, this.sizeX / 2, 20);
 
-	@Override
-	protected void actionPerformed(GuiButton button) throws IOException {
-		super.actionPerformed(button);
-		if (button == this.filter) {
-			Proxy.NETWORK.sendToServer(new PacketOpenFilterGui(this.source));
-			this.isRunning = false;
-		} else if (button == this.toggleRunning) {
-			isRunning = !isRunning;
-			button.displayString = isRunning ? "Stop" : "Start";
-			Proxy.NETWORK.sendToServer(new PacketSetQuarryState(source, isRunning));
-		} else if (button == this.confirm) {
-			try {
-				Proxy.NETWORK.sendToServer(new PacketSetMask(
-						new Filter(enchantsList.getText(), Integer.valueOf(dimensionList.getText()), new BitSet()),
-						source));
-				this.isRunning = false;
-			} catch (NumberFormatException e) {
-			}
-		}
-	}
+        this.dimensionList = new GuiTextField(id++, fontRenderer, startX + this.sizeX / 2, startY + 60, this.sizeX / 2,
+                20);
+    }
 
-	protected void keyTyped(char typedChar, int keyCode) throws IOException {
-		this.enchantsList.textboxKeyTyped(typedChar, keyCode);
-		this.dimensionList.textboxKeyTyped(typedChar, keyCode);
-		if (keyCode == 1) {
-			this.mc.player.closeScreen();
-		}
-	}
+    @Override
+    protected void actionPerformed(GuiButton button) throws IOException {
+        super.actionPerformed(button);
+        if (button == this.filter) {
+            Proxy.NETWORK.sendToServer(new PacketOpenFilterGui(this.source));
+            this.isRunning = false;
+        } else if (button == this.toggleRunning) {
+            isRunning = !isRunning;
+            button.displayString = isRunning ? "Stop" : "Start";
+            Proxy.NETWORK.sendToServer(new PacketSetQuarryState(source, isRunning));
+        } else if (button == this.confirm) {
+            try {
+                Proxy.NETWORK.sendToServer(new PacketSetMask(
+                        new Filter(enchantsList.getText(), Integer.valueOf(dimensionList.getText()), new BitSet()),
+                        source));
+                this.isRunning = false;
+            } catch (NumberFormatException e) {
+            }
+        }
+    }
 
-	/**
-	 * Called when the mouse is clicked. Args : mouseX, mouseY, clickedButton
-	 */
-	protected void mouseClicked(int mouseX, int mouseY, int mouseButton) throws IOException {
-		super.mouseClicked(mouseX, mouseY, mouseButton);
-		this.enchantsList.mouseClicked(mouseX, mouseY, mouseButton);
-		this.dimensionList.mouseClicked(mouseX, mouseY, mouseButton);
-	}
+    protected void keyTyped(char typedChar, int keyCode) throws IOException {
+        this.enchantsList.textboxKeyTyped(typedChar, keyCode);
+        this.dimensionList.textboxKeyTyped(typedChar, keyCode);
+        if (keyCode == 1) {
+            this.mc.player.closeScreen();
+        }
+    }
 
-	@Override
-	public boolean doesGuiPauseGame() {
-		return false;
-	}
+    /**
+     * Called when the mouse is clicked. Args : mouseX, mouseY, clickedButton
+     */
+    protected void mouseClicked(int mouseX, int mouseY, int mouseButton) throws IOException {
+        super.mouseClicked(mouseX, mouseY, mouseButton);
+        this.enchantsList.mouseClicked(mouseX, mouseY, mouseButton);
+        this.dimensionList.mouseClicked(mouseX, mouseY, mouseButton);
+    }
 
-	public BlockPos getSource() {
-		return this.source;
-	}
+    @Override
+    public boolean doesGuiPauseGame() {
+        return false;
+    }
+
+    public BlockPos getSource() {
+        return this.source;
+    }
 }
