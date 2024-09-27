7b69dde5146705d3f9649dd65d49487c76dcfedc
Raine Stone <samstone9001@gmail.com>
2024-09-26 20:52:09 -0400

    Fix NPE when loading malformed quarries

--- src/main/java/stone/hyperquarry/common/DropList.java
+++ src/main/java/stone/hyperquarry/common/DropList.java
@@ -31,7 +31,8 @@ import java.util.stream.DoubleStream;
 
 @Desugar
 public record DropList(Item[] items, short[] metas, double[] weights, int cost) {
-	private static final Map<String, DropList> MASTER_LISTS = new HashMap<>();
+
+    private static final Map<String, DropList> MASTER_LISTS = new HashMap<>();
 
     public static final int COST_PER_ITEM = 20000;
 
@@ -41,8 +42,8 @@ public record DropList(Item[] items, short[] metas, double[] weights, int cost) 
      * @return the cost, in RF, of generating a single item
      */
     public SharedStateDiscreteSampler init() {
-		return AliasMethodDiscreteSampler.of(RandomSource.XO_RO_SHI_RO_64_S.create(), weights);
-	}
+        return AliasMethodDiscreteSampler.of(RandomSource.XO_RO_SHI_RO_64_S.create(), weights);
+    }
 
     /**
      * Generates a random stack with the given count of items
@@ -51,12 +52,10 @@ public record DropList(Item[] items, short[] metas, double[] weights, int cost) 
      * @param count the stack size of the item stack
      * @return the stack randomly generated based on the weights
      */
-	public ItemStack getStack(int index, byte count) {
-		return new ItemStack(this.items[index], count, this.metas[index]);
-	}
-	
-    
-	
+    public ItemStack getStack(int index, byte count) {
+        return new ItemStack(this.items[index], count, this.metas[index]);
+    }
+
     /**
      * Creates a new drop list
      * 
@@ -65,11 +64,11 @@ public record DropList(Item[] items, short[] metas, double[] weights, int cost) 
      * @param dimension the numerical id of the dimension
      * @return
      */
-	public static DropList of(String enchants, int dimension) {
-		return MASTER_LISTS.computeIfAbsent(enchants + '_' + dimension, (id) -> {
+    public static DropList of(String enchants, int dimension) {
+        return MASTER_LISTS.computeIfAbsent(enchants + '_' + dimension, (id) -> {
             return loadDimensionDrops(id);
-		});
-	}
+        });
+    }
 
     /**
      * Creates a new drop list and automatically filters it based on the filter
@@ -77,63 +76,63 @@ public record DropList(Item[] items, short[] metas, double[] weights, int cost) 
      * @param dimension the numerical id of the dimension
      * @param filter    a bit set where each bit corresponds to a item being
      *                  included
-     * @return the new drop list
+     * @return the new drop list, null if filter was invalid
      */
     public static DropList of(Filter filter) {
-        return of(filter.enchants(), filter.dimension()).filter(filter.mask());
+        DropList maybeNull = of(filter.enchants(), filter.dimension());
+            return maybeNull != null ? maybeNull.filter(filter.mask()) : null;
     }
 
-	private static DropList loadDimensionDrops(String id) {
+    private static DropList loadDimensionDrops(String id) {
         ClassLoader classloader = Thread.currentThread().getContextClassLoader();
-        InputStream is = classloader.getResourceAsStream("stats/"+id+".txt");
+        InputStream is = classloader.getResourceAsStream("stats/" + id + ".txt");
         if (is == null)
-        	return null;
-		Pattern pattern = Pattern.compile("(.+?):(.+?):(\\d+?)-(\\d\\.\\d+(?:E-?)?\\d+)");
-		Map<Item, Int2DoubleMap> item2meta2weight = new HashMap<>();
-        try (Scanner scanner = new Scanner(is))
-        {
-			NavigableSet<Triple<Item, Short, Double>> entries = new TreeSet<>((triple1, triple2) -> {
-				double diff = triple1.getRight() - triple2.getRight();
-				return diff > 0 ? 1 : diff < 0 ? -1 : 0;
-			});
-			while (scanner.hasNextLine()) {
-				Matcher matcher = pattern.matcher(scanner.nextLine());
-				if (matcher.find()) {
-					Item item = Item.REGISTRY.getObject(new ResourceLocation(matcher.group(1), matcher.group(2)));
-					short meta = Short.valueOf(matcher.group(3));
-					double weight = Double.valueOf(matcher.group(4));
-					entries.add(Triple.of(item, meta, weight));
-				}
-			}
-			
+            return null;
+        Pattern pattern = Pattern.compile("(.+?):(.+?):(\\d+?)-(\\d\\.\\d+(?:E-?)?\\d+)");
+        Map<Item, Int2DoubleMap> item2meta2weight = new HashMap<>();
+        try (Scanner scanner = new Scanner(is)) {
+            NavigableSet<Triple<Item, Short, Double>> entries = new TreeSet<>((triple1, triple2) -> {
+                double diff = triple1.getRight() - triple2.getRight();
+                return diff > 0 ? 1 : diff < 0 ? -1 : 0;
+            });
+            while (scanner.hasNextLine()) {
+                Matcher matcher = pattern.matcher(scanner.nextLine());
+                if (matcher.find()) {
+                    Item item = Item.REGISTRY.getObject(new ResourceLocation(matcher.group(1), matcher.group(2)));
+                    short meta = Short.valueOf(matcher.group(3));
+                    double weight = Double.valueOf(matcher.group(4));
+                    entries.add(Triple.of(item, meta, weight));
+                }
+            }
+
             return DropList.of(entries);
-		}
-	}
-	
-	public static DropList of(NavigableSet<Triple<Item, Short, Double>> entries) {
-		int length = entries.size();
+        }
+    }
+
+    public static DropList of(NavigableSet<Triple<Item, Short, Double>> entries) {
+        int length = entries.size();
         Item[] items = new Item[length];
         short[] metas = new short[length];
         double[] weights = new double[length];
         for (int i = 0; i < length; i++) {
-        	Triple<Item, Short, Double> entry = entries.pollLast();
-        	items[i] = entry.getLeft();
-        	metas[i] = entry.getMiddle();
-        	weights[i] = entry.getRight();
+            Triple<Item, Short, Double> entry = entries.pollLast();
+            items[i] = entry.getLeft();
+            metas[i] = entry.getMiddle();
+            weights[i] = entry.getRight();
         }
-        
+
         return new DropList(items, metas, weights, COST_PER_ITEM);
-	}
+    }
 
     private DropList filter(BitSet mask) {
         double[] weights = new double[this.weights.length];
         for (int i = mask.nextSetBit(0); i != -1; i = mask.nextSetBit(i + 1)) {
-        	weights[i] = this.weights[i];
+            weights[i] = this.weights[i];
         }
-        
+
         double oldTotal = DoubleStream.of(this.weights).sum();
         double newTotal = DoubleStream.of(weights).sum();
-        
+
         return new DropList(this.items, this.metas, weights, (int) (this.cost / (newTotal / oldTotal)));
     }
 }
--- src/main/java/stone/hyperquarry/common/TileEntityQuarry.java
+++ src/main/java/stone/hyperquarry/common/TileEntityQuarry.java
@@ -26,40 +26,39 @@ public class TileEntityQuarry extends TileEntity implements ITickable, IEnergySt
     private long mined = 0;
     public static final String IS_RUNNING = "is_running";
     private boolean isRunning = false;
-	private DropList drops;
-	public static final String FILTER = "filter";
-	private Filter filter;
+    private DropList drops;
+    public static final String FILTER = "filter";
+    private Filter filter;
 
-	public TileEntityQuarry() {
-	}
-	@Override
-	public void update() {
+    public TileEntityQuarry() {
+    }
+
+    @Override
+    public void update() {
         if (this.world.isRemote || !isRunning)
             return;
         if (energy < this.drops.cost())
             return;
-		TileEntity maybeEntity = this.world.getTileEntity(pos.up());
-		if (maybeEntity != null) {
-			IItemHandler handler = maybeEntity.getCapability(CapabilityItemHandler.ITEM_HANDLER_CAPABILITY, EnumFacing.DOWN);
-			if (handler != null) {
+        TileEntity maybeEntity = this.world.getTileEntity(pos.up());
+        if (maybeEntity != null) {
+            IItemHandler handler = maybeEntity.getCapability(CapabilityItemHandler.ITEM_HANDLER_CAPABILITY,
+                    EnumFacing.DOWN);
+            if (handler != null) {
                 int toMine = energy / this.drops.cost();
                 this.mined += toMine;
                 energy = energy % this.drops.cost();
                 // bypass the stack limit cause why not
                 int stacks = toMine / Byte.MAX_VALUE;
                 byte leftover = (byte) (toMine % Byte.MAX_VALUE);
-                
+
                 int slot = 0;
                 int max = handler.getSlots();
                 ItemStack mined;
-                while (stacks > 0)
-                {
+                while (stacks > 0) {
                     mined = this.drops.getStack(this.sampler.sample(), Byte.MAX_VALUE);
-                    while ((mined = handler.insertItem(slot, mined, false)).getCount() > 0)
-                    {
+                    while ((mined = handler.insertItem(slot, mined, false)).getCount() > 0) {
                         slot++;
-                        if (slot >= max)
-                        {
+                        if (slot >= max) {
                             refundEnergy(stacks, leftover);
                             return;
                         }
@@ -67,65 +66,60 @@ public class TileEntityQuarry extends TileEntity implements ITickable, IEnergySt
                     stacks--;
                 }
                 mined = this.drops.getStack(this.sampler.sample(), leftover);
-                while ((mined = handler.insertItem(slot, mined, false)).getCount() > 0)
-                {
+                while ((mined = handler.insertItem(slot, mined, false)).getCount() > 0) {
                     slot++;
-                    if (slot >= max)
-                    {
+                    if (slot >= max) {
                         refundEnergy(stacks, leftover);
                         return;
                     }
                 }
-			}
-		}
-	}
+            }
+        }
+    }
 
     private void refundEnergy(int stacks, int leftover) {
         this.mined -= stacks * Byte.MAX_VALUE + leftover;
         try {
-            this.energy = Math.addExact(this.energy,  Math.multiplyExact(stacks * Byte.MAX_VALUE + leftover, this.drops.cost()));
-    } catch (ArithmeticException e) {
-        this.energy = Integer.MAX_VALUE;
-    }
+            this.energy = Math.addExact(this.energy,
+                    Math.multiplyExact(stacks * Byte.MAX_VALUE + leftover, this.drops.cost()));
+        } catch (ArithmeticException e) {
+            this.energy = Integer.MAX_VALUE;
+        }
     }
 
     @Override
-	public int receiveEnergy(int maxReceive, boolean simulate) {
-        if (!simulate)
-        {
-            try
-            {
+    public int receiveEnergy(int maxReceive, boolean simulate) {
+        if (!simulate) {
+            try {
                 this.energy = Math.addExact(this.energy, maxReceive);
-        } catch (ArithmeticException e)
-            {
+            } catch (ArithmeticException e) {
                 int toAdd = Integer.MAX_VALUE - this.energy;
                 this.energy = Integer.MAX_VALUE;
                 return toAdd;
             }
         }
         return maxReceive;
-	}
+    }
 
-	@Override
-	public int extractEnergy(int maxExtract, boolean simulate) {
-		return 0;
-	}
-	
-	
+    @Override
+    public int extractEnergy(int maxExtract, boolean simulate) {
+        return 0;
+    }
 
-	@Override
-	public void onLoad() {
-		super.onLoad();
-		if (filter == null) {
-			this.filter = new Filter("drops", 0, new BitSet());
-		}
-		this.drops = DropList.of(filter);
-		if (isRunning) {
-			this.sampler = this.drops.init();
-		}
-	}
+    @Override
+    public void onLoad() {
+        super.onLoad();
+        if (filter == null)
+            this.filter = new Filter("drops", 0, new BitSet());
+        this.drops = DropList.of(filter);
+        if (this.drops == null)
+            this.drops = DropList.of(new Filter("drops", 0, new BitSet()));
+        if (isRunning) {
+            this.sampler = this.drops.init();
+        }
+    }
 
-	@Override
+    @Override
 	public void readFromNBT(NBTTagCompound compound) {
 		super.readFromNBT(compound);
 		this.mined = compound.getLong(MINED);
@@ -134,29 +128,29 @@ public class TileEntityQuarry extends TileEntity implements ITickable, IEnergySt
 		this.isRunning = compound.getBoolean(IS_RUNNING);
 	}
 
-	@Override
-	public NBTTagCompound writeToNBT(NBTTagCompound compound) {
-		super.writeToNBT(compound);
-		compound.setLong(MINED, mined);
-		compound.setInteger(ENERGY, energy);
-		compound.setTag(FILTER, filter.writeToNBT(new NBTTagCompound()));
-		compound.setBoolean(IS_RUNNING, isRunning);
-		return compound;
-	}
+    @Override
+    public NBTTagCompound writeToNBT(NBTTagCompound compound) {
+        super.writeToNBT(compound);
+        compound.setLong(MINED, mined);
+        compound.setInteger(ENERGY, energy);
+        compound.setTag(FILTER, filter.writeToNBT(new NBTTagCompound()));
+        compound.setBoolean(IS_RUNNING, isRunning);
+        return compound;
+    }
 
-	@Override
-	public int getEnergyStored() {
+    @Override
+    public int getEnergyStored() {
         return energy;
-	}
+    }
 
-	@Override
-	public int getMaxEnergyStored() {
+    @Override
+    public int getMaxEnergyStored() {
         return Integer.MAX_VALUE;
-	}
+    }
 
-	@Override
+    @Override
     public boolean hasCapability(net.minecraftforge.common.capabilities.Capability<?> capability,
-        @Nullable net.minecraft.util.EnumFacing facing) {
+            @Nullable net.minecraft.util.EnumFacing facing) {
         if (capability == CapabilityEnergy.ENERGY)
             return true;
         return super.hasCapability(capability, facing);
@@ -170,47 +164,47 @@ public class TileEntityQuarry extends TileEntity implements ITickable, IEnergySt
     }
 
     @Override
-	public boolean canExtract() {
+    public boolean canExtract() {
         return false;
-	}
+    }
 
-	@Override
-	public boolean canReceive() {
+    @Override
+    public boolean canReceive() {
         return true;
-	}
+    }
 
     public void setFilter(Filter filter) {
         if (isRunning)
             isRunning = false;
         DropList temp = DropList.of(filter);
         if (temp != null) {
-        	this.drops = temp;
-        	this.filter = filter;
+            this.drops = temp;
+            this.filter = filter;
         }
     }
 
-	public Filter getFilter() {
-		return this.filter;
-	}
+    public Filter getFilter() {
+        return this.filter;
+    }
 
     public long getMined() {
         return this.mined;
     }
-    
-    public boolean isRunning() { return this.isRunning; }
+
+    public boolean isRunning() {
+        return this.isRunning;
+    }
 
     public void setRunning(boolean isRunning) {
-        if (this.isRunning != isRunning)
-        {
+        if (this.isRunning != isRunning) {
             this.isRunning = isRunning;
-            if (isRunning)
-            {
+            if (isRunning) {
                 this.sampler = this.drops.init();
             }
         }
     }
-	public int getCost() {
-		// TODO Auto-generated method stub
-		return this.drops.cost();
-	}
+
+    public int getCost() {
+        return this.drops.cost();
+    }
 }
